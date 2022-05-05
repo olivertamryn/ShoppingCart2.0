93e030a44c5c4120e319b725a2810ef8de385988
olivertamryn <4270497@myuwc.ac.za>
2022-05-05 11:56:41 +0200

    commit message

---
+++ ShoppingCartSln/ShoppingLibrary1/ShoppingCartItems.cs
@@ -0,0 +1,48 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingLibrary
+{
+    public class ShoppingCartItem
+    {
+        public string Name;
+        public double Price;
+        public ShoppingCartItem(string name, double price)
+        {
+            Name = name;
+            Price = _price;
+        }
+        public string _productName
+        {
+            get { return _productName; }
+            set { _productName = value; }
+        }
+        public double _price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
